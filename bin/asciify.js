a8cc4ffe67eb4cc8c24cf083264f8ce44e5e9060
Vishruth <vishruthdevan@gmail.com>
2021-11-02 15:11:07 +0530

    Add option for specific font

--- bin/asciify.js
+++ bin/asciify.js
@@ -3,11 +3,12 @@ import fetch from "node-fetch";
 const check = async (text, options) => {
   const url = "https://artii.herokuapp.com/make?text=";
   if (options.font) {
-    console.log(options.font);
+    const res = await fetch(`${url}${encodeURI(text)}&font=${options.font}`);
+    return console.log(await res.text());
   }
   if (text) {
     const res = await fetch(url + encodeURI(text));
-    console.log(await res.text());
+    return console.log(await res.text());
   }
 };
 
