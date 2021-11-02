75bfa10eb67a31ec859edc4164940a5f8668cf13
Vishruth <vishruthdevan@gmail.com>
2021-11-02 12:26:48 +0530

    Add asciify and list commands to cli

--- bin/art.js
+++ bin/art.js
@@ -2,10 +2,22 @@
 
 import program from "commander";
 
-program.version("1.0.0");
-program.name("art");
-program.description("Access the ASCIIfy API to generate text in ASCII fonts");
+program
+  .version("1.0.0")
+  .name("art")
+  .description("Access the ASCIIfy API to generate text in ASCII fonts")
+  .usage("<command>");
+
+program
+  .command("asciify")
+  .argument("<text>", "Text to ASCIIfy")
+  .option("-f, --font", "Get text in a specific font")
+  .action(console.log("asciify text"))
+  .description("Gets ASCIIfied text from the prints the output");
+
+program
+  .command("list")
+  .action(console.log("list fonts"))
+  .description("Retrieve a list of all available available");
 
 program.parse(process.argv);
-
-console.log("Hello World!");
